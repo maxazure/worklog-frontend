6cf97e2b75608833b9da222beef14b783691fc01
maxazure <maxazure@gmail.com>
2020-10-14 20:43:18 +1300

    added users

---
+++ src/views/users/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="users-container">
+    <div class="users-text">name: {{ name }}</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "users",
+  computed: {
+    ...mapGetters(["name"])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.users {
+  &-container {
+    margin: 30px;
+  }
+  &-text {
+    font-size: 30px;
+    line-height: 46px;
+  }
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -36,18 +36,18 @@ module.exports = {
       warnings: false,
       errors: true
     },
-    // proxy: {
-    //   // change xxx-api/login => mock/login
-    //   // detail: https://cli.vuejs.org/config/#devserver-proxy
-    //   [process.env.VUE_APP_BASE_API]: {
-    //     target: `http://shop.cdb99.com:8088/api`,
-    //     changeOrigin: true,
-    //     pathRewrite: {
-    //       ['^' + process.env.VUE_APP_BASE_API]: ''
-    //     }
-    //   }
-    // },
-    before: require('./mock/mock-server.js')
+    proxy: {
+      // change xxx-api/login => mock/login
+      // detail: https://cli.vuejs.org/config/#devserver-proxy
+      [process.env.VUE_APP_BASE_API]: {
+        target: `http://192.168.178.52:5000/api`,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    },
+    // before: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
